0b7aa02f82df3ed85742dd139593856aa7b96dd2
Clint Hiles <clinthiles1992@gmail.com>
2024-07-08 16:24:13 -0700

    generate randome string

--- express_server.js
+++ express_server.js
@@ -5,11 +5,22 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+const generateRandomString = function() {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let result = "";
+  for (const i = 0; i < 6; i++) {
+    result += chars.charAt(Math.floor(Math.random() + chars.length));
+  }
+  return result;
+};
+
 
 const urlDatabase = {
   b2xVn2: "http//:www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
@@ -25,6 +36,11 @@ app.get("/urls/:id", (req, res) => {
   const longURL = urlDatabase[id];
   const templateVars = { id: id, longURL: longURL };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 
