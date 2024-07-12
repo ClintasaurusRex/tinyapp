b0c3eaac9dc04971a23c19c4ac3767d85e5e060f
Clint Hiles <clinthiles1992@gmail.com>
2024-07-11 17:32:54 -0700

    testing testing

--- test/helperTest.js
+++ test/helperTest.js
@@ -1,7 +1,6 @@
 const { assert } = require('chai');
 
-const { getUserByEmail } = require('../helpers/helper.js');
-
+const { getUserByEmail } = require('../helpers.js');
 const testUsers = {
   "userRandomID": {
     id: "userRandomID",
