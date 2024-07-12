041b10dd88aca6e91f4e65d63ca3c718862fabe3
Clint Hiles <clinthiles1992@gmail.com>
2024-07-12 12:52:06 -0700

    running the tests

--- test/urlsForUserTest.js
+++ test/urlsForUserTest.js
@@ -1,5 +1,6 @@
 const { assert } = require('chai');
 const { urlsForUser } = require('../helpers.js');
+const { urlDatabase } = require('../data/url_Database.js');
 // Assume that urlsForUser is already implemented and imported
 // const { urlsForUser } = require('./path_to_helper_function'); // Uncomment and adjust path as necessary
 
@@ -22,6 +23,34 @@ describe('urlsForUser', function() {
     const result = urlsForUser('user1', urlDatabase);
 
     // Assert that the result matches the expected output
+    assert.deepEqual(result, expectedOutput);
+  });
+  it("should return an empty object if the user has no urls", function() {
+    const urlDatabase = {
+      "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userId: "user1" },
+      "9sm5xK": { longURL: "http://www.google.com", userId: "user2" },
+      "a1b2c3": { longURL: "http://www.example.com", userId: "user1" }
+    };
+    const result = urlsForUser('user3', urlDatabase);
+
+    assert.deepEqual(result, {});
+  });
+  it("should return an empty object if there are no urls in the urlDatabase", function() {
+    const emptyDatabase = {};
+    const result = urlsForUser('user4', urlDatabase);
+    assert.deepEqual(result, {});
+  });
+  it('should not return urls that dont belong to the specified user', function() {
+    const urlDatabase = {
+      "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userId: "user1" },
+      "9sm5xK": { longURL: "http://www.google.com", userId: "user2" },
+      "a1b2c3": { longURL: "http://www.example.com", userId: "user1" }
+    };
+    const expectedOutput = {
+      "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userId: "user1" },
+      "a1b2c3": { longURL: "http://www.example.com", userId: "user1" }
+    };
+    const result = urlsForUser('user1', urlDatabase);
     assert.deepEqual(result, expectedOutput);
   });
 });
